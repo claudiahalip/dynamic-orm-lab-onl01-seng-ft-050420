cb7fb79c5b2129db891afa2b3808bb802abf896b
claudia v <claudia4ro@yahoo.com>
2020-06-06 03:12:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,10 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
   
-  
+  self.column_names.each do | column_names |
+    
+    attr_accessor column_name.to_sym
+    
+  end
 
 end
