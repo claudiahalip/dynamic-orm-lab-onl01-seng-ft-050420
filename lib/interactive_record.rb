d179c23e29c6f1a210ed5ee56f9c1edc0f657822
claudia v <claudia4ro@yahoo.com>
2020-06-06 03:56:02 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -72,10 +72,10 @@ class InteractiveRecord
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
    
-  def self.find_by_name
+  def self.find_by_name(name)
     sql = <<-SQL
     SELECT * FROM self.table_name_for_insert
-    WHERE 
+    WHERE #{"name} = name
   end
   
   def self.find_by
