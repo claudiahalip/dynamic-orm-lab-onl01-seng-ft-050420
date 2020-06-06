fda18ebfb4fdcc95f65e55fe26a6b21ec5d1c211
claudia v <claudia4ro@yahoo.com>
2020-06-06 02:55:39 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,4 +3,38 @@ require 'active_support/inflector'
 
 class InteractiveRecord
   
+  def self.table_name 
+    self.to_s.downcase.pluralize
+  end 
+  
+  def self.column_names 
+    
+    DB[:conn].results_as_hash = true 
+    
+    sql = "PRAGMA table_info ('#{table_name}')"
+    
+    table_info = DB[:conn].execute(sql)
+    
+    column_names = []
+    table_info.each do |column|
+      column_names << column["name"]
+    end
+    column_names.compact
+  end
+  
+  
+  self.column_names.each do | column_name |
+    attr_accessor column_name.to_sym 
+  end
+  
+  def initialize (h_arr = {})
+    h_arr.each do | key, value |
+      self.send("#{key}",value)
+    end 
+    self
+  end 
+   
+  
+  
+  
 end
