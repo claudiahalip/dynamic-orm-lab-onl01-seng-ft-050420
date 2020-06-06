c04b01331ee1c3a052b7cf4591c7dd9241ccaeb1
claudia v <claudia4ro@yahoo.com>
2020-06-06 03:58:10 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -73,7 +73,7 @@ class InteractiveRecord
   end
    
   def self.find_by_name(name)
-    sql = "SELECT * FROM self.table_name_for_insert
+    sql = "SELECT * FROM #{self.table_name}
     WHERE name = '#{name}'"
     DB[:conn].execute(sql)
   end
