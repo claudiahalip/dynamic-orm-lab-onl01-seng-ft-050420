a7b646d9df3085a19ca8f2901fb2dd47f5faf42d
claudia v <claudia4ro@yahoo.com>
2020-06-06 03:13:09 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -23,9 +23,9 @@ class InteractiveRecord
   end
   
   
-  self.column_names.each do | column_name |
-    attr_accessor column_name.to_sym 
-  end
+  # self.column_names.each do | column_name |
+  #   attr_accessor column_name.to_sym 
+  # end
   
   def initialize (h_arr = {})
     h_arr.each do | key, value |
