7e6ab9ed86ac8863796107310f2cd064d34f8c0c
claudia v <claudia4ro@yahoo.com>
2020-06-06 03:30:44 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -34,13 +34,13 @@ class InteractiveRecord
     self.class.table_name
   end
   
-  def column_names_for_insert
+  def col_names_for_insert
     self.class.column_names.delete_if { | column | column = "id"}.join(", ")
   end 
   
   
   def save 
-    sql = "INSERT INTO #{table_name_for_insert} (#{column_names_for_insert}) VALUES (#{values_for_insert})"
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
     DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
